b7033a73d0eb3cae07ef835a9176f0184f76d39d
Carlos Velasquez <cv.velasquez@gmail.com>
2016-03-21 10:21:16 -0700

    intial commet

---
+++ app.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+$('start').on('click(function(event) {
+  /* Act on the event */
+  var gameStart = $
+});')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })
