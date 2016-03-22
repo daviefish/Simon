2a38abcf76c27ecb4d20e1ce01d9d3b5dd987028
Carlos Velasquez <cv.velasquez@gmail.com>
2016-03-22 16:47:32 -0700

    timedBlinks

--- app.js
+++ app.js
@@ -21,20 +21,31 @@ var blink = function(square) {
   $(square).addClass('flash');
   setTimeout(function() {
     $(square).removeClass('flash');
-  }, 500)
-}
+  }, 200);
+};
 
 // making block blink seperately (and random?)
 var blinkArray = function() {
-  for (var i = 0; i < arr.length; i++){
-    Math.random((blink(arr[i]), 1700))
+  // for (var i = 0; i < arr.length; i++){
+  //   // Math.random.delay(3000, ((blink(arr[i]), 1700)));
+  //   Math.floor( Math.random( blink(arr[i]) + 4 ) );
+  // }
+  var counter = 0;
+  var timerID = setInterval(function() {
+    blink(arr[counter]);
+    counter++;
+  }, 200);
+
+  if(counter === 4) {
+    return timerID;
   }
-}
+
+};
 
 // DOM and node association
 $('.blocks').each(function(index, element) {
 
-})
+});
 
 
 
