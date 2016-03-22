01d636abc0cd35bd74b31a0ed63cb907501b7824
Carlos Velasquez <cv.velasquez@gmail.com>
2016-03-22 12:32:41 -0700

    flash

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 
 
-$(document).ready(function(){
 
 
-//  $('#start').on('click', function(){
-//      $('#container').hide();
-//     $('#gameboard').slideUp();
-//  });
+
 
 $('#start').on('click', function(){
       $('#container').removeClass('visible').addClass('hidden');
@@ -15,6 +11,29 @@ $('#start').on('click', function(){
 
 });
 
+//make array out of gameboard pieces
+var fourSquares = $('.gameboard').children();
+var arr = $.makeArray(fourSquares);
+console.log(fourSquares.length);
+
+// blocks turning white
+var blink = function() {
+  $('.blocks').addClass('flash');
+  setTimeout(function() {
+    $('.blocks').removeClass('flash');
+  }, 500)
+}
+
+// making block blink seperately (and random?)
+var blinkArray = function() {
+  for (var i = 0; i < fourSquares.length; i++);
+
+}
+
+// DOM and node association
+$('.blocks').each(function(index, element) {
+
+})
 
 
 
@@ -23,8 +42,6 @@ $('#start').on('click', function(){
 
 
 
-
-});
 
 
 
